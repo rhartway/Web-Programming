7e04914fd1df9df71f1637340c4fbef4509d45e5
Derek9132 <derek9132@gmail.com>
2024-10-03 12:53:09 -0400

    added chat and togglechat

--- frontend/scripts/meetingRoom.js
+++ frontend/scripts/meetingRoom.js
@@ -1,10 +1,24 @@
+let membersVisible = false;
+let chatVisible = false;
+
 
 document.getElementById("toggleMembers").onclick = function() {
     console.log("show members");
-    if (document.getElementById("memberList").style.display == "none") {
+    if (membersVisible == false) {
     document.getElementById("memberList").style.display = "flex";
     }
     else {
         document.getElementById("memberList").style.display = "none";
     }
+    membersVisible = !membersVisible;
+}
+
+document.getElementById("toggleChat").onclick = function() {
+    if (chatVisible == false) {
+        document.getElementById("chatBox").style.display = "flex";
+    }
+    else {
+        document.getElementById("chatBox").style.display = "none";
+    }
+    chatVisible = !chatVisible;
 }
