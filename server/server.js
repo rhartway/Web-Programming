b5107c6e62bbf3846b5f94ceb8b4850737435230
clam <cindyberri@gmail.com>
2024-12-03 21:11:03 -0500

    motions, motions chatboard additions

--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
 import express from 'express'
 import cors from 'cors'
 import { getUser, makeUser } from './database.js';
+import { saveChatMessage, getChatMessages } from './database.js';
+import { getMotion, getMotions, makeMotion } from './database.js';
+
 import bodyParser from 'body-parser';
 import token from 'jsonwebtoken'
+import { Server } from "socket.io";
 
+import { createServer } from "node:http";
 
+  
+import {
+    handleChatConnection,
+    setIoInstance,
+} from "../controllers/chatController.js"; 
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+//add server and io for socket
+const server = createServer(app);
+const io = new Server(server)
 
 const port = 8080;
 
@@ -23,6 +37,26 @@ const port = 8080;
  */
 
 app.use(cors({origin: "http://127.0.0.1:5500"}));
+
+//set io instance
+setIoInstance(io);
+// Chat connection handling
+io.on("connection", (socket) => {
+    socket.on('joinRoom', async (motionID) => {
+        socket.join(motionID);
+        const messages = await getChatMessages(motionID);
+        socket.emit('messageHistory', messages);
+    });
+
+    socket.on('chatMessage', async ({ motionID, sender, message }) => {
+        await saveChatMessage(motionID, sender, message);
+        io.to(motionID).emit('message', { sender, message });
+    });
+
+    handleChatConnection(socket);
+});
+
+
 
 
 /**
@@ -80,7 +114,43 @@ app.put("/profile/update", (req, res) => {
 
 //
 
-// Create + Join + Delete Meeting Room
+/* Motion stuff
+ * like getting motions and making them
+ * and stuff
+ */
+app.get('/api/motions', async (req, res) => {
+    try {
+        const motions = await getMotions();
+        res.json(motions);
+    } catch (err) {
+        res.status(500).send("Error retrieving motions");
+    }
+});
+
+//TODO: API for making a motion, retrieving specific motion
+
+// Create + Join + Delete Meeting/Chat Room
+//route to chatroom
+app.get('/chatroom/:motionID', (req, res) => {
+  const motionID = req.params.motionID;
+  res.sendFile(__dirname + '/client/chatroom.html');
+});
+
+io.on("connection", (socket) => {
+    socket.on('joinRoom', (motionID) => {
+      socket.join(motionID);
+    });
+  
+    socket.on('chatMessage', ({ motionID, message }) => {
+      io.to(motionID).emit('message', message);
+    });
+  
+    handleChatConnection(socket);
+  });
+
+
+
+
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
